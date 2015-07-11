db99629d25cadf1e29925eb9c545862115ce8ddf
decisactor <decisact@aol.com>
2015-07-10 23:07:34 -0700

    Create Apps.sh

---
+++ Apps.sh
@@ -0,0 +1,11 @@
+# Linux
+
+#    Download Visual Studio Code for Linux
+#    Make a new folder and extract VSCode-linux-x64.zip inside that folder
+#    Double click on Code to run Visual Studio Code
+
+#    Tip: If you want to run VS Code from the terminal, create the following link substituting /path/to/vscode/Code with the absolute path to the Code executable
+
+# sudo ln -s /path/to/vscode/Code /usr/local/bin/code
+sudo ln -s /home/decisactor/Documents/VSCode-linux-x64/Code /usr/local/bin/code
+# Now, you can simply type code . in any folder to start editing files in that folder.
