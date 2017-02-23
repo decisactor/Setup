4dc3206aed976235d0fdf5547b46acc186142ba0
decisactor <decisact@aol.com>
2017-02-22 23:44:44 -0800

    Update Install.sh

--- TensorFlow/Ubuntu/Install.sh
+++ TensorFlow/Ubuntu/Install.sh
@@ -1,37 +1,25 @@
 
-# 1. Install pip and virtualenv by issuing the following command:
 
-sudo apt-get install python-pip python-dev python-virtualenv 
+# Ensure proper protobuf dependencies by issuing one of the following commands:
 
-# 2. Create a virtualenv environment by issuing the following command:
+#sudo pip uninstall tensorflow # for Python 2.7
+sudo pip3 uninstall tensorflow # for Python 3.n
 
-virtualenv --system-site-packages targetDirectory 
+# Install TensorFlow by invoking one of the following commands:
 
-# The targetDirectory specifies the top of the virtualenv tree. Our instructions assume that targetDirectory is ~/tensorflow, but you may choose any directory.
+#pip install tensorflow      # Python 2.7; CPU support (no GPU support)
+pip3 install tensorflow     # Python 3.n; CPU support (no GPU support)
+#pip install tensorflow-gpu  # Python 2.7;  GPU support
+#pip3 install tensorflow-gpu # Python 3.n; GPU support 
 
-# 3. Activate the virtualenv environment by issuing one of the following commands:
+# If the preceding command runs to completion, you should now validate your installation.
 
-source ~/tensorflow/bin/activate # If using bash, ksh, sh, or 
-source ~/tensorflow/bin/activate.csh  # If using csh 
+# (Optional.) If Step 2 failed, install the latest version of TensorFlow by issuing a command of the following format:
 
-# The preceding source command should change your prompt to the following:
-# (tensorflow)$ 
+#sudo pip  install --upgrade TF_BINARY_URL   # Python 2.7
+#sudo pip3 install --upgrade TF_BINARY_URL   # Python 3.N 
 
-# 4. Issue one of the following commands to install TensorFlow in the active virtualenv environment:
+#where TF_BINARY_URL identifies the URL of the TensorFlow Python package. The appropriate value of TF_BINARY_URL depends on the operating system, Python version, and GPU support. Find the appropriate For example, if you are installing TensorFlow for Linux, Python version 3.4, and CPU-only support, the command to install TensorFlow is as follows:
 
-#pip install --upgrade tensorflow      # for Python 2.7
-pip3 install --upgrade tensorflow     # for Python 3.n
-#pip install --upgrade tensorflow-gpu  # for Python 2.7 and GPU
-#pip3 install --upgrade tensorflow-gpu # for Python 3.3 and GPU
-
-# If the preceding command succeeds, skip Step 5. If the preceding command fails, perform Step 5.
-
-# 5. (Optional) If Step 4 failed (typically because you invoked a pip version lower than 8.1), install TensorFlow in the active virtualenv environment by issuing a command of the following format:
-
- (tensorflow)$ pip install --upgrade TF_PYTHON_URL   # Python 2.7
- (tensorflow)$ pip3 install --upgrade TF_PYTHON_URL  # Python 3.N 
-
-# where TF_PYTHON_URL identifies the URL of the TensorFlow Python package. The appropriate value of TF_PYTHON_URLdepends on the operating system, Python version, and GPU support. Find the appropriate value for TF_PYTHON_URL for your system here. For example, if you are installing TensorFlow for Linux, Python version 3.4, and CPU-only support, issue the following command to install TensorFlow in the active virtualenv environment:
-
-pip3 install --upgrade \
-https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-py3-none-any.whl
+#sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-py3-none-any.whl
+ 
